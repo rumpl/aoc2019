3729d30f8519e40b35947dc2bfc888cb35f76443
Djordje Lukic <djordje.lukic@docker.com>
2019-12-01 11:45:47 +0100

    01.2

--- 01/main.py
+++ 01/main.py
@@ -3,7 +3,12 @@ import fileinput
 
 
 def fuel(mass):
-    return int(math.floor(int(mass) / 3) - 2)
+    if mass <= 0:
+        return 0
+    f = int(math.floor(int(mass) / 3) - 2)
+    if f < 0:
+        f = 0
+    return f + fuel(f)
 
 
 total = 0
